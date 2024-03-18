91900edec53ba0a899bdd057bb037398e8d6f46b
Boyd Querubin <boyd@ezlynkna.com>
2024-03-18 13:41:53 -0600

    first commit

---
+++ starter.py
@@ -0,0 +1,63 @@
+# first_name = 'Boyd'
+# last_name = 'Querubin'
+
+# age = 32
+# age = 12
+# age = 15
+# age = "twenty-six"
+
+# print(age)
+# print(first_name, last_name)
+
+# if age >= 18:
+#     print("I am an adult")
+# elif age < 13:
+#     print("I am a child")
+# else:
+#     print("I am an alien")
+
+
+#     print("Hello")
+
+# favorite_numbers = [1, 2, 3, 4 ,5, "hello", True, 2.0]
+
+# print(favorite_numbers)
+
+# for number in  favorite_numbers:
+#     print(number)
+
+# def sum(num_1, num_2):
+#     return num_1 + num_2
+
+# result = sum(1, 2)
+# print(result)
+
+# open_file = open("FinalGrades.csv")
+# total_a = 0
+# total_b = 0
+# total_c = 0
+
+# for line in open_file:
+#     line = line.strip()
+#     values = line.split(',')
+#     print(values)
+#     for value in values:
+#         if value == "A":
+#             total_a += 1
+#         elif value == "B":
+#             total_b += 1
+#         elif value == "C":
+#             total_c += 1
+
+# print("A's:", total_a)
+# print("B's:", total_b)
+# print("C's:", total_c)
+
+# for line in open_file:
+#     line = line.strip()
+#     values = line.split(",")
+#     print(values[2: 5])
+
+# open_file.close()
+
+# prime_number = float(1)
